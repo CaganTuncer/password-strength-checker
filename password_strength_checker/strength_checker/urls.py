9c6a3eca00ea730958d2c1a1104cf7ce318dfd69
Çağan Tuncer <cagan.tuncer@ug.bilkent.edu.tr>
2025-01-04 17:33:08 +0300

    main page start

--- password_strength_checker/strength_checker/urls.py
+++ password_strength_checker/strength_checker/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.check_password, name="check_password"),
 ]
--- password_strength_checker/strength_checker/views.py
+++ password_strength_checker/strength_checker/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import PasswordForm
 
 def index(request):
+    print("why are you here")
     return HttpResponse("started...")
+
+def check_password(request):
+    strength = None
+    if request.method == 'POST':
+        form = PasswordForm(request.POST)
+        if form.is_valid():
+            password = form.cleaned_data['password']
+            # Implement your password strength logic here
+            strength = 10
+    else:
+        form = PasswordForm()
+    return render(request, 'strength_checker/strength_checker.html', {'form': form, 'strength': strength})
 
 # Create your views here.

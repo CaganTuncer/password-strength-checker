1cea885c3ed29bb36c7a6a5078d4e8d886f4add0
yagmur <yagmur.okan@edu.sait.ca>
2025-01-04 08:14:15 -0700

    Password checking logic

--- password_strength_checker/strength_checker/views.py
+++ password_strength_checker/strength_checker/views.py
@@ -12,7 +12,24 @@ def check_password(request):
         form = PasswordForm(request.POST)
         if form.is_valid():
             password = form.cleaned_data['password']
-            # Implement your password strength logic here
+            if len(password) < 8:
+                print("Password must be at least 8 characters long.")
+
+            if not re.search("[A-Z]", password):
+                 print("Password must contain at least one upper case letter.")
+
+            if not re.search("[a-z]", password):
+                print("Password must contain at least one lower case letter")
+
+            if not re.search("[0-9]", password):
+                print("Password must contain at least one digit.")
+
+            if not re.search("!@#$%^&*(),.?\":{}|<>", password):
+                print("Password must contain at least one special character.")   
+
+            else:
+                print("Password is set!")
+
             strength = 10
     else:
         form = PasswordForm()

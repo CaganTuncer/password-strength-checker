67b0a04a7708185f281bf110830041645b7363bd
Çağan Tuncer <cagan.tuncer@ug.bilkent.edu.tr>
2025-01-04 18:20:45 +0300

    logic fix

--- password_strength_checker/strength_checker/views.py
+++ password_strength_checker/strength_checker/views.py
@@ -1,3 +1,5 @@
+import re
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import PasswordForm
@@ -12,25 +14,31 @@ def check_password(request):
         form = PasswordForm(request.POST)
         if form.is_valid():
             password = form.cleaned_data['password']
+            strength = 100
             if len(password) < 8:
                 print("Password must be at least 8 characters long.")
+                strength -= 20
 
             if not re.search("[A-Z]", password):
-                 print("Password must contain at least one upper case letter.")
+                print("Password must contain at least one upper case letter.")
+                strength -= 20
 
             if not re.search("[a-z]", password):
                 print("Password must contain at least one lower case letter")
+                strength -= 20
 
             if not re.search("[0-9]", password):
                 print("Password must contain at least one digit.")
+                strength -= 20
 
             if not re.search("!@#$%^&*(),.?\":{}|<>", password):
-                print("Password must contain at least one special character.")   
+                print("Password must contain at least one special character.")
+                strength -= 20   
 
             else:
                 print("Password is set!")
 
-            strength = 10
+            
     else:
         form = PasswordForm()
     return render(request, 'strength_checker/strength_checker.html', {'form': form, 'strength': strength})

386f9673d54a4befdcef7c0cbd8b7d6e1fb87cd0
Çağan Tuncer <cagan.tuncer@ug.bilkent.edu.tr>
2025-01-13 18:50:42 +0300

    Routing Fix + Error Messages On Screen
    The project now routes to /strength_checker/ on startup and displays what is wrong with the password entered as a list.

--- password_strength_checker/password_strength_checker/urls.py
+++ password_strength_checker/password_strength_checker/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("strength_checker/", include("strength_checker.urls")),
     path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url="/strength_checker/")),
 ]
--- password_strength_checker/strength_checker/views.py
+++ password_strength_checker/strength_checker/views.py
@@ -10,37 +10,49 @@ def index(request):
 
 def check_password(request):
     strength = None
+    strings = []
+    length = 0
     if request.method == 'POST':
+        
         form = PasswordForm(request.POST)
         if form.is_valid():
             password = form.cleaned_data['password']
             strength = 100
             if len(password) < 8:
+                strings.append("Password must be at least 8 characters long.")
                 print("Password must be at least 8 characters long.")
                 strength -= 20
 
             if not re.search("[A-Z]", password):
+                strings.append("Password must contain at least one upper case letter.")
                 print("Password must contain at least one upper case letter.")
                 strength -= 20
 
             if not re.search("[a-z]", password):
+                strings.append("Password must contain at least one lower case letter")
                 print("Password must contain at least one lower case letter")
                 strength -= 20
 
             if not re.search("[0-9]", password):
+                strings.append("Password must contain at least one digit.")
                 print("Password must contain at least one digit.")
                 strength -= 20
 
-            if not re.search("!@#$%^&*(),.?\":{}|<>", password):
+            if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+                strings.append("Password must contain at least one special character.")
                 print("Password must contain at least one special character.")
                 strength -= 20   
 
             else:
+                strings.append("Password is set!")
                 print("Password is set!")
 
+            length = len(strings)
             
+
+    
     else:
-        form = PasswordForm()
-    return render(request, 'strength_checker/strength_checker.html', {'form': form, 'strength': strength})
+        form = [PasswordForm()]
+    return render(request, 'strength_checker/strength_checker.html', {'form': form, 'strength': strength, 'strings': strings, 'length': length})
 
 # Create your views here.
